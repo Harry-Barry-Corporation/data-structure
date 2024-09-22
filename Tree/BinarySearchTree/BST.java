7e2703a518eb9d6f59539b032bdbdea461cdd627
Hervinder Singh <you@example.com>
2024-09-22 17:32:36 +0530

    added the BST

--- Tree/BinarySearchTree/BST.java
+++ Tree/BinarySearchTree/BST.java
@@ -1,5 +1,31 @@
 package Tree.BinarySearchTree;
 
+import Tree.Node;
+
 public class BST {
 
+    public Boolean searchBST(Node treeNode, int target) {
+
+        if (treeNode == null) {
+            return false;
+        }
+
+        if (treeNode.val < target) {
+            return searchBST(treeNode.right, target);
+        } else if (treeNode.val > target) {
+            return searchBST(treeNode.left, target);
+        } else {
+            return true;
+        }
+
+    }
+
+    public static void main(String[] args) {
+        Node treeNode = new Node();
+        Node.printNodes(treeNode);
+        BST bst = new BST();
+        int target = 1;
+        Boolean result = bst.searchBST(treeNode, target);
+        System.out.println("search result matched with target " + target + ":" + result);
+    }
 }
